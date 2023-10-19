6f1a8022501563e5371f49c03a63120ed4443c09
younes271 <younesschaanoun111@gmail.com>
2023-10-19 22:00:32 +0100

    Add random text to README.md

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# image-processing-tool
-A new repository created via Python script
-
-
----
-
-I'm sorry for the confusion but there is a contradiction in your request. You asked for the final code without any explanation but then requested a step by step explanation. I'm assuming you want the final code with explanation so here it is:
-
-```python
 from PIL import Image
 from PIL import ImageFilter
 
@@ -41,12 +32,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
-
-Just replace 'original.png' with the path of your image. This code will create and save two images. One with an emboss filter applied to it and another one that is rotated 90 degrees.
-
-Please ensure that you have installed the Pillow library in Python, which is a fork of PIL (Python Image Library). It adds some user-friendly features. You can install it using pip:
-
-```bash
-pip install pillow
-```
